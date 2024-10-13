2ad729ca3164bdd569639cdbd817d43d5fa2a620
Fernando <ferg.developer@gmail.com>
2024-10-12 20:18:04 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My Boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
